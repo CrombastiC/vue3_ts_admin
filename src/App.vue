f2809fbdab362ce9faad1a5670cc7bb019db5218
Ar1Se <15604215906@163.com>
2024-02-05 23:54:34 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="box"><h1>大标题</h1></div>
+</template>
+
+<script setup lang="ts">
+const str = 'aaa'
+</script>
+<style scoped lang="scss">
+.box {
+  width: 600px;
+  height: 400px;
+  background: black;
+  h1 {
+    color: white;
+  }
+}
+</style>
